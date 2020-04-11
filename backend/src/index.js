d4d47235958eeeba521eb2e265d7e7dc90ab43b4
César O. Araújo <oaraujocesar@MacBook-Pro-de-Cesar.local>
2020-04-11 14:00:02 -0300

    Feat: Creating the main routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,34 @@ const app = express();
 
 const port = 3333;
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello, world' });
+app.get('/projects', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2'
+  ]);
+});
+
+app.post('/projects', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'  
+  ]);
+});
+
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'  
+  ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 2',
+    'Projeto 3'  
+  ]);
 });
 
 app.listen(port, () => {
