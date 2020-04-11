dffdd07c1acd9410284bad1a9ffcd022352db349
César O. Araújo <oaraujocesar@MacBook-Pro-de-Cesar.local>
2020-04-11 13:35:35 -0300

    Feat: Setting up Nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = 3333;
+
 app.get('/', (req, res) => {
   return res.json({ message: 'Hello, world' });
 });
 
-app.listen(3333, () => {
-  console.log('Your Back-end is running on port 3333!');
+app.listen(port, () => {
+  console.log(`⚡️ Back-end is running on port ${port}!`);
 });
