0f87dd1490bbe74b62f0e97cbc32b0856e2afa91
César O. Araújo <oaraujocesar@MacBook-Pro-de-Cesar.local>
2020-04-12 17:27:11 -0300

    Feat: Using props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+
+  const [projects, setProjects] = useState(["Desenvolvimento de apps", "Front-end Web"]);
+
+  function handleAddProject() {
+
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+  };
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {
+          projects.map((project, index) => <li key={index}>{project}</li>)
+        }
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
     </>
   );
 }
