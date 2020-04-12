08cc915c3e78cc720fe51ccd19e40a76e142d640
César O. Araújo <oaraujocesar@MacBook-Pro-de-Cesar.local>
2020-04-12 18:03:27 -0300

    Feat: Webpack css and image support and integration with api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
 
 import Header from './components/Header';
 
 function App() {
 
-  const [projects, setProjects] = useState(["Desenvolvimento de apps", "Front-end Web"]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
 
   function handleAddProject() {
-
     setProjects([...projects, `Novo projeto ${Date.now()}`]); 
-
   };
 
   return (
@@ -18,7 +25,7 @@ function App() {
 
       <ul>
         {
-          projects.map((project, index) => <li key={index}>{project}</li>)
+          projects.map(project => <li key={project.id}>{project.title}</li>)
         }
       </ul>
 
